025978a15397f50fb02521dd2c6dae6c4531055b
Raey-Abera <ab.raey@gmail.com>
2020-11-13 22:34:26 -0500

    formatting and deleting unwated files

--- getSum/app.js
+++ getSum/app.js
@@ -2,24 +2,24 @@
 
 // Note: a and b are not ordered!
 
-// Examples
+// Examples:
 // getSum(1, 0) == 1   // 1 + 0 = 1
 // getSum(1, 2) == 3   // 1 + 2 = 3
 // getSum(0, 1) == 1   // 0 + 1 = 1
-// getSum(1, 1) == 1   // 1 Since both are same
+// getSum(1, 1) == 1   // 1 Bc both are same
 // getSum(-1, 0) == -1 // -1 + 0 = -1
 // getSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
 
 const getSum = (num1, num2) => {
-    if(num1 === num2)return num2
+    if (num1 === num2) return num2
     let result = 0;
-    let smaller = Math.min(num1,num2)
-    const larger = Math.max(num1,num2)
-    for (let i = smaller; i <= larger; i ++ ){
-     result += i
+    let smaller = Math.min(num1, num2)
+    const larger = Math.max(num1, num2)
+    for (let i = smaller; i <= larger; i++) {
+        result += i
     }
     return result
-  } 
+}
 
 console.log(getSum(1, 0)) //1
 console.log(getSum(1, 2)) //3
